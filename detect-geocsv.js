cc7adb4c5c3ce6434e41c25c0c2451c1e1df2b17
jakepruitt <jake@mapbox.com>
2015-05-21 14:55:57 -0400

    Finished initial draft

--- detect-geocsv.js
+++ detect-geocsv.js
@@ -1,16 +1,14 @@
-var geocsvinfo = require('geocsv-info');
-
 module.exports = function(buf) {
     var lines = buf.toString().split(/\r\n|\r|\n/g);
 
     var i = 0;
 
-    while (lines[i] == '')
+    while (lines[i] === '')
         i++;
 
     var firstline = lines[i];
 
-    var separator = geocsvinfo.detectSeparator(firstline);
+    var separator = detectSeparator(firstline);
 
     var headers = firstline.split(separator);
 
@@ -18,10 +16,79 @@ module.exports = function(buf) {
         return header.replace(/"/g, '');
     });
 
-    var geometryField = geocsvinfo.detectGeometryField(headers);
+    var geometryField = detectGeometryField(headers);
     if (!geometryField) {
         return false;
     }
 
     return true;
+};
+
+function detectSeparator(csv_line) {
+    // implemented like: https://github.com/mapnik/mapnik/blob/f42805a5321d42f59b447a70f459058cf2c6cd5c/plugins/input/csv/csv_datasource.cpp#L209
+
+    var separators = [',','\t','|',';'];
+    var counts = separators.map(function(separator){
+        return csv_line.split(separator).length - 1;
+    });
+
+    var best = null, max = 0;
+    for (var i = 0; i < 4; i++) {
+        if (counts[i] > max) {
+            max = counts[i];
+            best = separators[i];
+        }
+    }
+    return best;
+}
+
+function detectGeometryField(fieldnames) {
+    // adapted from: https://github.com/mapnik/mapnik/blob/f42805a5321d42f59b447a70f459058cf2c6cd5c/plugins/input/csv/csv_datasource.cpp#L293
+    var result;
+    for (var i = 0, n = fieldnames.length; i < n; i++) {
+        var name = fieldnames[i].toLowerCase();
+        if (name === 'wkt' || name === 'geom' || name === 'geometry') {
+            result = {
+                id: i,
+                name: fieldnames[i],
+                encoding: "WKT"
+            }
+            break;
+        }
+        if (name === 'geojson') {
+            result = {
+                id: i,
+                name: fieldnames[i],
+                encoding: "GeoJSON"
+            };
+            break;
+        }
+        if (name === 'x'
+            || name === 'lon'
+            || name === 'lng'
+            || name === 'long'
+            || name === 'longitude')
+        {
+
+            result = result || {
+                id: {},
+                name: {},
+                encoding: "PointFromColumns"
+            }
+            continue;
+        }
+        if (name === 'y'
+            || name === 'lat'
+            || name === 'latitude')
+        {
+
+            result = result || {
+                id: {},
+                name: {},
+                encoding: "PointFromColumns"
+            }
+            continue;
+        }
+    }
+    return result;
 };
--- test/index.test.js
+++ test/index.test.js
@@ -4,19 +4,178 @@ var fs = require('fs');
 var testData = path.dirname(require.resolve('mapnik-test-data'));
 var isgeocsv = require('../detect-geocsv.js');
 
-tape('[CSV] Sniffing file with empty rows: should return csv filetype and omnivore protocol', function(assert) {
-    var filepath = path.resolve('./test/data/valid-empty_rows.csv');
-    var buffer;
-    try {
-        fs.statSync(filepath);
-        buffer = new Buffer(512);
-        var fd = fs.openSync(filepath, 'r');
-        fs.readSync(fd, buffer, 0, 512, 0);
-        fs.closeSync(fd);
-    } catch (err) {
-        return assert.end(err);
-    }
-    var result = isgeocsv(buffer);
-    assert.ok(result);
+tape('[CSV] Detecting valid CSV files: should return true', function(assert) {
+    var folder = testData + '/data/csv/';
+    var files = fs.readdirSync(folder);
+
+    files.forEach(function(filename) {
+        var filepath = folder + filename;
+        var buffer;
+        try {
+            fs.statSync(filepath);
+            buffer = new Buffer(512);
+            var fd = fs.openSync(filepath, 'r');
+            fs.readSync(fd, buffer, 0, 512, 0);
+            fs.closeSync(fd);
+        } catch (err) {
+            return assert.end(err);
+        }
+        var result = isgeocsv(buffer);
+        assert.ok(result, filename + 'comes back true');
+    });
+    assert.end();
+});
+
+tape('[GEOJSON] Detecting non-CSV files: should return false', function(assert) {
+    var folder = testData + '/data/geojson/';
+    var files = fs.readdirSync(folder);
+
+    files.forEach(function(filename) {
+        var filepath = folder + filename;
+        var buffer;
+        try {
+            fs.statSync(filepath);
+            buffer = new Buffer(512);
+            var fd = fs.openSync(filepath, 'r');
+            fs.readSync(fd, buffer, 0, 512, 0);
+            fs.closeSync(fd);
+        } catch (err) {
+            return assert.end(err);
+        }
+        var result = isgeocsv(buffer);
+        assert.ok(!result, filename + 'comes back false');
+    });
+    assert.end();
+});
+
+tape('[GEOTIFF] Detecting non-CSV files: should return false', function(assert) {
+    var folder = testData + '/data/geotiff/';
+    var files = fs.readdirSync(folder);
+
+    files.forEach(function(filename) {
+        var filepath = folder + filename;
+        var buffer;
+        try {
+            fs.statSync(filepath);
+            buffer = new Buffer(512);
+            var fd = fs.openSync(filepath, 'r');
+            fs.readSync(fd, buffer, 0, 512, 0);
+            fs.closeSync(fd);
+        } catch (err) {
+            return assert.end(err);
+        }
+        var result = isgeocsv(buffer);
+        assert.ok(!result, filename + 'comes back false');
+    });
+    assert.end();
+});
+
+tape('[GPX] Detecting non-CSV files: should return false', function(assert) {
+    var folder = testData + '/data/gpx/';
+    var files = fs.readdirSync(folder);
+
+    files.forEach(function(filename) {
+        var filepath = folder + filename;
+        var buffer;
+        try {
+            fs.statSync(filepath);
+            buffer = new Buffer(512);
+            var fd = fs.openSync(filepath, 'r');
+            fs.readSync(fd, buffer, 0, 512, 0);
+            fs.closeSync(fd);
+        } catch (err) {
+            return assert.end(err);
+        }
+        var result = isgeocsv(buffer);
+        assert.ok(!result, filename + ' comes back false');
+    });
+    assert.end();
+});
+
+tape('[KML] Detecting non-CSV files: should return false', function(assert) {
+    var folder = testData + '/data/kml/';
+    var files = fs.readdirSync(folder);
+
+    files.forEach(function(filename) {
+        var filepath = folder + filename;
+        var buffer;
+        try {
+            fs.statSync(filepath);
+            buffer = new Buffer(512);
+            var fd = fs.openSync(filepath, 'r');
+            fs.readSync(fd, buffer, 0, 512, 0);
+            fs.closeSync(fd);
+        } catch (err) {
+            return assert.end(err);
+        }
+        var result = isgeocsv(buffer);
+        assert.ok(!result, filename + ' comes back false');
+    });
+    assert.end();
+});
+
+tape('[topojson] Detecting non-CSV files: should return false', function(assert) {
+    var folder = testData + '/data/topojson/';
+    var files = fs.readdirSync(folder);
+
+    files.forEach(function(filename) {
+        var filepath = folder + filename;
+        var buffer;
+        try {
+            fs.statSync(filepath);
+            buffer = new Buffer(512);
+            var fd = fs.openSync(filepath, 'r');
+            fs.readSync(fd, buffer, 0, 512, 0);
+            fs.closeSync(fd);
+        } catch (err) {
+            return assert.end(err);
+        }
+        var result = isgeocsv(buffer);
+        assert.ok(!result, filename + ' comes back false');
+    });
+    assert.end();
+});
+
+tape('[vrt] Detecting non-CSV files: should return false', function(assert) {
+    var folder = testData + '/data/vrt/';
+    var files = fs.readdirSync(folder);
+
+    files.forEach(function(filename) {
+        var filepath = folder + filename;
+        var buffer;
+        try {
+            fs.statSync(filepath);
+            buffer = new Buffer(512);
+            var fd = fs.openSync(filepath, 'r');
+            fs.readSync(fd, buffer, 0, 512, 0);
+            fs.closeSync(fd);
+        } catch (err) {
+            return assert.end(err);
+        }
+        var result = isgeocsv(buffer);
+        assert.ok(!result, filename + ' comes back false');
+    });
+    assert.end();
+});
+
+tape('[zip] Detecting non-CSV files: should return false', function(assert) {
+    var folder = testData + '/data/zip/';
+    var files = fs.readdirSync(folder);
+
+    files.forEach(function(filename) {
+        var filepath = folder + filename;
+        var buffer;
+        try {
+            fs.statSync(filepath);
+            buffer = new Buffer(512);
+            var fd = fs.openSync(filepath, 'r');
+            fs.readSync(fd, buffer, 0, 512, 0);
+            fs.closeSync(fd);
+        } catch (err) {
+            return assert.end(err);
+        }
+        var result = isgeocsv(buffer);
+        assert.ok(!result, filename + ' comes back false');
+    });
     assert.end();
 });
