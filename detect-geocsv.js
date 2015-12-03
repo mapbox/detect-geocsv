c3982afa3a73f6fcc8edc6349f48ea50b42e1ce0
Tom MacWright <tom@macwright.org>
2015-12-03 13:17:05 -0500

    Require both latitude & longitude for CSV points
    Also refactors this module to be more succinct

--- detect-geocsv.js
+++ detect-geocsv.js
@@ -1,91 +1,40 @@
 module.exports = function(buf) {
-    var lines = buf.toString().split(/\r\n|\r|\n/g);
+    var lines = buf.toString()
+        .split(/\r\n|\r|\n/g)
+        .filter(function(line) {
+            return line !== '';
+        });
 
-    lines = lines.filter(function(line) {
-        return line !== '';
-    });
-
-    if (!lines.length) {
-        return false
-    }
-
-    var firstline = lines[0];
-
-    var separator = detectSeparator(firstline);
-
-    var headers = firstline.split(separator).map(function(header) {
-        return header.replace(/"/g, '');
-    });
-
-    var geometryField = detectGeometryField(headers);
-    return !!geometryField;
+    return !!lines.length &&
+        !!detectGeometryField(lines[0]
+            .split(detectSeparator(lines[0]))
+            .map(function(header) {
+                return header.replace(/"/g, '');
+            }));
 };
 
 function detectSeparator(csv_line) {
     // implemented like: https://github.com/mapnik/mapnik/blob/f42805a5321d42f59b447a70f459058cf2c6cd5c/plugins/input/csv/csv_datasource.cpp#L209
+    return [',','\t','|',';'].map(function(separator) {
+        return [separator, csv_line.split(separator).length - 1];
+    }).sort(function(a, b) {
+        return b[1] - a[1];
+    })[0][0];
+}
 
-    var separators = [',','\t','|',';'];
-    var counts = separators.map(function(separator) {
-        return csv_line.split(separator).length - 1;
+function hasOne(list, items) {
+    return list.some(function(item) {
+        return items.indexOf(item) !== -1;
     });
-
-    var best = null, max = 0;
-    for (var i = 0; i < 4; i++) {
-        if (counts[i] > max) {
-            max = counts[i];
-            best = separators[i];
-        }
-    }
-    return best;
 }
 
 function detectGeometryField(fieldnames) {
     // adapted from: https://github.com/mapnik/mapnik/blob/f42805a5321d42f59b447a70f459058cf2c6cd5c/plugins/input/csv/csv_datasource.cpp#L293
-    var result;
-    for (var i = 0, n = fieldnames.length; i < n; i++) {
-        var name = fieldnames[i].toLowerCase();
-        if (name === 'wkt' || name === 'geom' || name === 'geometry') {
-            result = {
-                id: i,
-                name: fieldnames[i],
-                encoding: 'WKT'
-            };
-            break;
-        }
-        if (name === 'geojson') {
-            result = {
-                id: i,
-                name: fieldnames[i],
-                encoding: 'GeoJSON'
-            };
-            break;
-        }
-        if (name === 'x'
-            || name === 'lon'
-            || name === 'lng'
-            || name === 'long'
-            || name === 'longitude')
-        {
+    var lowerCaseNames = fieldnames.map(function(name) {
+        return name.toLowerCase();
+    });
 
-            result = result || {
-                id: {},
-                name: {},
-                encoding: 'PointFromColumns'
-            };
-            continue;
-        }
-        if (name === 'y'
-            || name === 'lat'
-            || name === 'latitude')
-        {
-
-            result = result || {
-                id: {},
-                name: {},
-                encoding: 'PointFromColumns'
-            };
-            continue;
-        }
-    }
-    return result;
+    return hasOne(lowerCaseNames, ['wkt', 'geom', 'geometry', 'geojson']) ||
+        (hasOne(lowerCaseNames, ['x', 'lon', 'lng', 'long', 'longitude']) &&
+        hasOne(lowerCaseNames, ['y', 'lat', 'latitude']));
 }
